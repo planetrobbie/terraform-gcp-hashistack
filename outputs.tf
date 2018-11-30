faaae0c659c5642ec01045ffdd7c4dc17ef5efb6
Sébastien Braun <sebbraun@gmail.com>
2018-11-30 19:02:54 +0100

    output return a list now

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "public_ip" {
-  value = "Instances: ${element(google_compute_instance.vm.*.network_interface.0.access_config.0.assigned_nat_ip)}"
+  value = ["${google_compute_instance.vm.*.network_interface.0.access_config.0.assigned_nat_ip}"]
 }
