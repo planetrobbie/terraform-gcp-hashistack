3a023fdb3d50a984cedcbb7a5ecc7f739615b716
Sébastien Braun <sebbraun@gmail.com>
2018-11-30 22:52:40 +0100

    Vault IPs output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "public_ip" {
+output "consul IPs" {
   value = ["${google_compute_instance.vm.*.network_interface.0.access_config.0.assigned_nat_ip}"]
+}
+
+output "Vault IPs" {
+  value = ["${google_compute_instance.vault.*.network_interface.0.access_config.0.assigned_nat_ip}"]
 }
