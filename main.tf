8ed39efce9d251ea9160cef771e3de8fb69b1f7f
Sébastien Braun <sebbraun@gmail.com>
2018-11-30 22:46:38 +0100

    two vault VMs

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "google" {
 #  credentials = "${file(var.account_file_path)}"
 }
 
+# using cloud-init to install Python needed for Ansible
 data "template_file" "userdata" {
     template = "${file("templates/userdata.tpl")}"
     vars {
@@ -38,4 +39,34 @@ resource "google_compute_instance" "vm" {
   }
 
   count = 3
+}
+
+resource "google_compute_instance" "vault" {
+  name         = "ansible-vault-${count.index}"
+  machine_type = "${var.instance_type}"
+  zone         = "${var.region_zone}"
+  allow_stopping_for_update = true
+
+  tags = ["vault"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.image}"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata {
+    sshKeys = "${var.ssh_user}:${var.ssh_pub_key}"
+    user-data = "${data.template_file.userdata.rendered}"
+  }
+
+  count = 2
 }
