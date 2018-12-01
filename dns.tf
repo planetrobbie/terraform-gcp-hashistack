0e0a2dd75588317a6594dcf07a9f5ac0a754ccb1
Sébastien Braun <sebbraun@gmail.com>
2018-12-01 10:22:07 +0100

    all DNS entries

--- dns.tf
+++ dns.tf
@@ -3,6 +3,36 @@ resource "google_dns_managed_zone" "dns_zone" {
   dns_name = "${var.gcp_dns_domain}"
 }
 
+resource "google_dns_record_set" "consul-1" {
+  name = "c1.${google_dns_managed_zone.dns_zone.dns_name}"
+  type = "A"
+  ttl  = "${var.ttl}"
+
+  managed_zone = "${google_dns_managed_zone.dns_zone.name}"
+
+  rrdatas = ["${google_compute_instance.vm.0.network_interface.0.access_config.0.assigned_nat_ip}"]
+}
+
+resource "google_dns_record_set" "consul-2" {
+  name = "c2.${google_dns_managed_zone.dns_zone.dns_name}"
+  type = "A"
+  ttl  = "${var.ttl}"
+
+  managed_zone = "${google_dns_managed_zone.dns_zone.name}"
+
+  rrdatas = ["${google_compute_instance.vm.1.network_interface.0.access_config.0.assigned_nat_ip}"]
+}
+
+resource "google_dns_record_set" "consul-3" {
+  name = "c3.${google_dns_managed_zone.dns_zone.dns_name}"
+  type = "A"
+  ttl  = "${var.ttl}"
+
+  managed_zone = "${google_dns_managed_zone.dns_zone.name}"
+
+  rrdatas = ["${google_compute_instance.vm.2.network_interface.0.access_config.0.assigned_nat_ip}"]
+}
+
 resource "google_dns_record_set" "vault-1" {
   name = "v1.${google_dns_managed_zone.dns_zone.dns_name}"
   type = "A"
