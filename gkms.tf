05835bc41335f75e1d767dca8f381c8b02ffab21
Sébastien Braun <sebbraun@gmail.com>
2018-12-03 19:37:08 +0100

    no more prevent destroy

--- gkms.tf
+++ gkms.tf
@@ -7,8 +7,4 @@ resource "google_kms_key_ring" "vault_keyring" {
 resource "google_kms_crypto_key" "vault_key" {
   name            = "${var.gkms_vault_key}"
   key_ring        = "${google_kms_key_ring.vault_keyring.self_link}"
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
