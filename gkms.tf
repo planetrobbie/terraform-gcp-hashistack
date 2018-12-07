cd52322a44e0941bc1d145a9dfdbebe65d938b34
Sébastien Braun <sebbraun@gmail.com>
2018-12-08 00:45:19 +0100

    Google KMS Service Account resource for Auto-unseal

--- gkms.tf
+++ gkms.tf
@@ -8,3 +8,17 @@ resource "google_kms_crypto_key" "vault_key" {
   name            = "${var.gkms_vault_key}"
   key_ring        = "${google_kms_key_ring.vault_keyring.self_link}"
 }
+
+resource "google_service_account" "kms_access" {
+  account_id   = "sb-vault-kms"
+  display_name = "sb-vault-kms Account"
+}
+
+resource "google_service_account_iam_binding" "kms-account-iam" {
+  service_account_id = "sb-vault-kms"
+  role        = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+
+  members = [
+    "serviceAccount:sb-vault-kms@sb-vault.iam.gserviceaccount.com",
+  ]
+}
